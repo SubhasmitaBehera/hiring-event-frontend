93a23b8fa7c4875a9352f87899b00fb2d9457619
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2021-07-18 22:19:54 +0530

    interview rounds model update

--- src/app/rounds/rounds.component.ts
+++ src/app/rounds/rounds.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rounds.component.css']
 })
 export class RoundsComponent implements OnInit {
-
+  interviewRoundTitle = "DETAILS OF Interview ";
   constructor() { }
 
   ngOnInit(): void {
